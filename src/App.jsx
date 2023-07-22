9cd1d70d1095ae8469cb5b2872ad48599a5bda37
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-22 18:57:02 +0300

    added logic to create and handle forms moved to hero.jsx and SignupConextProvider.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,15 @@
 import React, { useState } from 'react'
 
 import './App.css'
+import Hero from './Hero'
 
 function App() {
  
-  const [formData, setFormData] = useState({
-    name: '',
-    location: ''
-  })
-
-  function handleChange(event) {
-    const {name, value} = event.target
-    setFormData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [name]: value
-      }
-    })
-
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault()
-    fetch('http://localhost:3000/upload', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData)
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
-
-    setFormData({name: '', location: ''})
-  }
-
-  console.log(formData)
+  
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username:</label>
-        <input 
-           type="text"
-           name='name'
-           value={formData.name}
-           onChange={handleChange}
-            />
-        <label htmlFor="location">Location:</label>
-        <input 
-           type="text"
-           name='location'
-           value={formData.location}
-           onChange={handleChange}
-            />
-        <button>Submit</button>
-      </form>
+     <Hero/>
     </>
   )
 }
