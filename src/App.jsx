33367f7129e1dcc14e751874fc876d2830a02ddd
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-24 13:38:03 +0300

    added logic to get all images

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 
 import './App.css'
 import Hero from './Hero'
 
+import { signupContext } from './context/SignupContextProvider'
+
 function App() {
+
+  const { getImages } = useContext(signupContext)
  
   
 
   return (
     <>
      <Hero/>
+     <button onClick={() => getImages()}>Get images</button>
     </>
   )
 }
